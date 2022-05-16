56bff90b251274c438db49ee5db379f89b75e9aa
PC <elmeftouhi@extensionmedia.ma>
2022-05-16 22:13:26 +0100

    slide work

--- resources/views/home/partials/slide.blade.php
+++ resources/views/home/partials/slide.blade.php
@@ -1,18 +1,49 @@
 <div class="w-full flex gap-4 bg-white h-96">
-    <div class="flex-1 bg-red-200">
-        images 1
+    <div class="flex-1 grid place-items-center bg-red-200">
+        <div class="w-full flex justify-between h-12 items-center px-4">
+            <div class="py-1 px-2 bg-white text-gray-600 hover:text-gray-900 rounded-lg"><i class="fa-solid fa-angle-left"></i></div>
+            <div class="bg-white px-6 py-2 text-center rounded">
+                <div class="text-gray-400 text-sm">
+                    Category
+                </div>
+                <div class="text-lg text-black">
+                    Item Short Name
+                </div>
+            </div>
+            <div class="py-1 px-2 bg-white text-gray-600 hover:text-gray-900 rounded-lg"><i class="fa-solid fa-angle-right"></i></div>
+        </div>
     </div>
     <div class="flex flex-col h-full flex-1 gap-4">
         <div class="flex flex-1 gap-4">
-            <div class="flex-1 bg-gray-100">
-                image 1
+            <div class="grid place-items-center flex-1 bg-gray-100">
+                <div class="w-full flex justify-center h-12 items-center px-4">
+                    <div class="bg-white px-6 py-2 text-center rounded">
+                        <div class="text-lg text-black">
+                            Women
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="flex-1 bg-gray-300">
-                image 2
+            <div class="grid place-items-center flex-1 bg-gray-100">
+                <div class="w-full flex justify-center h-12 items-center px-4">
+                    <div class="bg-white px-6 py-2 text-center rounded">
+                        <div class="text-lg text-black">
+                            Men
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="flex-1 bg-gray-500">
-            image 3
+        <div class="grid place-items-center flex-1 bg-gray-500">
+            <div class="grid place-items-center flex-1 bg-gray-100">
+                <div class="w-full flex justify-center h-12 items-center px-4">
+                    <div class="bg-white px-6 py-2 text-center rounded">
+                        <div class="text-lg text-black">
+                            Accessories
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
