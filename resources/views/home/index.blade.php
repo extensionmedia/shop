cb6d520b0265373f291df5000e9b888fe225e921
PC <elmeftouhi@extensionmedia.ma>
2022-06-10 23:32:51 +0100

    group as section

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,24 +4,7 @@
     @include('home.partials.slide')
     @include('home.partials.infos')
 
-    <div class="mt-12">
-        <div class="flex justify-between items-center py-4">
-            <div class="font-bold text-black text-3xl">
-                Woman's
-            </div>
-            <div class="flex gap-8">
-                <a href="http://" class="text-gray-600 font-bold underline">New Arrivals</a>
-                <a href="http://" class="text-gray-600 hover:underline">Specials</a>
-                <a href="http://" class="text-gray-600 hover:underline">Bestsellers</a>
-                <a href="http://" class="text-gray-600 hover:underline">Most Viewed</a>
-                <a href="http://" class="text-gray-600 hover:underline">Featured Products</a>
-            </div>
-        </div>
-    </div>
-    <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
-        @include('components.product.item')
-        @include('components.product.item')
-        @include('components.product.item')
-        @include('components.product.item')
-    </div>
+    @include('components.section', ['category'=>"Women's", 'products'=>[1,2,3,4]])
+    @include('components.section', ['category'=>"Men's", 'products'=>[1,2,3,4]])
+
 @endsection
